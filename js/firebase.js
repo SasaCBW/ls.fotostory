2069f478cea95846a5fd330e730ef37032ff10f6
SasaCBW <sara.wendling@escola.pr.gov.br>
2026-06-20 13:23:33 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,67 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// 🔥 COLE AQUI SUA CONFIGURAÇÃO DO FIREBASE
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBkLBbm6gwbRfW16vA4YucU9MWQP9rtfCg",
+  authDomain: "ls-fotostory.firebaseapp.com",
+  projectId: "ls-fotostory",
+  storageBucket: "ls-fotostory.firebasestorage.app",
+  messagingSenderId: "281416299489",
+  appId: "1:281416299489:web:c0111bf6af5f463a7dd8a0",
+  measurementId: "G-G66MD22MW5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-
-// =============================
-// 🔵 LOGIN GOOGLE
-// =============================
-window.loginGoogle = async function () {
-  try {
-    await signInWithPopup(auth, provider);
-    window.location.href = "inicio.html";
-  } catch (error) {
-    alert("Erro login Google: " + error.message);
-  }
-};
-
-
-// =============================
-// 🟡 CRIAR CONTA
-// =============================
-window.cadastrar = async function () {
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
-
-  try {
-    await createUserWithEmailAndPassword(auth, email, senha);
-    alert("Conta criada com sucesso!");
-  } catch (error) {
-    alert(error.message);
-  }
-};
-
-
-// =============================
-// 🟢 LOGIN EMAIL
-// =============================
-window.loginEmail = async function () {
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
-
-  try {
-    await signInWithEmailAndPassword(auth, email, senha);
-    window.location.href = "inicio.html";
-  } catch (error) {
-    alert("Erro login: " + error.message);
-  }
-};
+const analytics = getAnalytics(app);
